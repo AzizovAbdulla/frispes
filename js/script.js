56b216954eb96c2699adb90b1a7193ebeee6b0ce
AzizovAbdulla <azizovabdulla01@gmail.com>
2022-05-29 16:25:20 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+
+});
